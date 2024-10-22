65c9ad2e2a553ab4b7bff681f9c97aa86533e1b3
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-22 16:01:52 +0200

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+// console.log('test');
+
+
+// In questo esercizio, dovrai creare una classe in JavaScript chiamata "Veicolo" con alcune proprietà e metodi.
+// La classe "Veicolo" dovrebbe avere le seguenti caratteristiche:
+    // marca
+    // anno
+    // colore
+    // porte
+    // carburante
+
+// Successivamente:
+// Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
+// Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
+// Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
+
+// BONUS
+// In questo bonus, dovrai estendere la classe "Veicolo" con una nuova classe chiamata "Automobile". La classe "Automobile" eredita tutte le proprietà e i metodi della classe "Veicolo" e ne aggiunge alcuni specifici per le automobili.
+// Successivamente:
+// Istanziamo una nuova Automobile (Opel, 2020, nera, con 4 porte, a benzina)
+// Stampiamo il numero di porte e il tipo di carburante.
+
+
+
+
